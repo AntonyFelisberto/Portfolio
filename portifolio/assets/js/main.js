a4060afa1a32db23b281f3cf89d627143e324358
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2023-04-01 15:48:08 -0300

    ajustando portifolio

--- portifolio/assets/js/main.js
+++ portifolio/assets/js/main.js
@@ -1,18 +1,21 @@
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+const navLink = document.querySelector(".nav_link")
+
 window.onload = function(){
     let divItens = document.querySelector("#certificados")
 
     fetch("../../json/dadosImages.json").then((response)=>{
         response.json().then((dados) =>{
             dados.certificados.map((certificados)=>{
-                divItens.innerHTML+="<img src='"+certificados.nome+"'/>"
+                divItens.innerHTML+="<div class='item'><img class='box-filme' alt='' srcset='' src='"+certificados.nome+"'/></div>"
             })
         })
     })
-}
 
-const navMenu = document.getElementById('nav-menu');
-const navToggle = document.getElementById('nav-toggle');
-const navClose = document.getElementById('nav-close');
+    navLink.forEach(n=> n.addEventListener("click",linkAction))
+}
 
 if(navToggle){
     navToggle.addEventListener("click",()=>{
@@ -26,13 +29,11 @@ if(navClose){
     })
 }
 
-const navLink = document.querySelector(".nav_link")
 
 function linkAction(){
     const navMenu = document.getElementById("nav-menu")
     navMenu.classList.remove("show-menu")
 }
-navLink.forEach(n=> n.addEventListener("click",linkAction))
 
 function toggleSkills(elem){
     const skillsContent = document.getElementsByClassName('skills_content')
@@ -47,3 +48,25 @@ function toggleSkills(elem){
         elem.parentNode.className = "skills_content skills_open"
     }
 }
+
+const modalViews = document.querySelectorAll('.services_modal'),
+      modalBtns = document.querySelectorAll('.services_button'),
+      modalCloses = document.querySelectorAll('.services_modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn,i)=>{
+    modalBtn.addEventListener('click',() =>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose)=>{
+    modalClose.addEventListener('click',()=>{
+        modalViews.forEach((modalView)=>{
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
