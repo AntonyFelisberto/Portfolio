c126d6fd9d3e53d859b787e05f2dbf8a07b1f3ca
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2024-05-08 15:21:24 -0300

    ajustes css e certificações

--- portifolio/assets/js/app.js
+++ portifolio/assets/js/app.js
@@ -1,6 +1,34 @@
-var app = angular.module("mainApp",[]);
+var app = angular.module('mainApp', ['pascalprecht.translate'])
 
-app.controller("myMainController",function($scope){
+app.config(['$translateProvider',
+  function($translateProvider) {
+
+    $translateProvider.useStaticFilesLoader({
+            prefix: 'assets/js/translation_',
+            suffix: '.json'
+        });
+        
+    $translateProvider.preferredLanguage("en");
+  }
+]);
+
+app.controller('myMainController', function msgController($scope,$translate) {
     $scope.idade = (new Date().getFullYear() - 2002);
-    $scope.tempoDeServicos = new Date().getFullYear() - 2022
-})
+    $scope.tempoDeServicos = new Date().getFullYear() - 2021
+
+    var _this = this;
+
+    this.versions = {
+      ajs: angular.version.full,
+      translate: $translate.versionInfo()
+    };
+    
+    this.currentLanguage = $translate.use();
+  
+    this.chooseLanguage = function (lang) {
+      $translate.use(lang)
+      .then(function (languageId) {
+        _this.currentLanguage = languageId;
+      });
+    };
+});
