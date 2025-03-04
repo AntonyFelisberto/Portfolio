40e095ea5605d31f13dbfcfad227ea31ffa8b9f8
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2025-03-03 21:47:49 -0300

    ajustes de estrutura html

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, QueryList, ViewChildren, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-services',
-  standalone: true,
-  imports: [],
   templateUrl: './services.component.html',
-  styleUrl: './services.component.css'
+  styleUrls: ['./services.component.css']
 })
-export class ServicesComponent {
+export class ServicesComponent implements AfterViewInit {
 
+  @ViewChildren('modalView') modals!: QueryList<ElementRef>;
+  @ViewChildren('modalButton') modalButtons!: QueryList<ElementRef>;
+  @ViewChildren('modalClose') modalCloses!: QueryList<ElementRef>;
+
+  ngAfterViewInit() {
+    this.modalButtons.forEach((button, index) => {
+      button.nativeElement.addEventListener('click', () => this.openModal(index));
+    });
+
+    this.modalCloses.forEach((close, index) => {
+      close.nativeElement.addEventListener('click', () => this.closeModal(index));
+    });
+  }
+
+  openModal(index: number) {
+    const modal = this.modals.toArray()[index];
+    if (modal) {
+      modal.nativeElement.style.opacity = '1';
+      modal.nativeElement.style.visibility = 'visible';
+    }
+  }
+
+  closeModal(index: number) {
+    const modal = this.modals.toArray()[index];
+    if (modal) {
+      modal.nativeElement.style.opacity = '0';
+      modal.nativeElement.style.visibility = 'hidden';
+    }
+  }
 }
--- src/to refactor/js/main.js
+++ src/to refactor/js/main.js
@@ -10,28 +10,6 @@ function linkAction(){
     navMenu.classList.remove("show-menu")
 }
 
-const modalViews = document.querySelectorAll('.services_modal'),
-      modalBtns = document.querySelectorAll('.services_button'),
-      modalCloses = document.querySelectorAll('.services_modal-close')
-
-let modal = function(modalClick){
-    modalViews[modalClick].classList.add('active-modal')
-}
-
-modalBtns.forEach((modalBtn,i)=>{
-    modalBtn.addEventListener('click',() =>{
-        modal(i)
-    })
-})
-
-modalCloses.forEach((modalClose)=>{
-    modalClose.addEventListener('click',()=>{
-        modalViews.forEach((modalView)=>{
-            modalView.classList.remove('active-modal')
-        })
-    })
-})
-
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive(){
